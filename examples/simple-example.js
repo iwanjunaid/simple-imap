3b9f1907a0d447be223e16b1752675b9f52e2b68
Iwan Junaid <iwan.junaid@gmail.com>
2014-10-10 11:22:37 +0700

    Initial commit.

---
+++ examples/simple-example.js
@@ -0,0 +1,38 @@
+var SimpleImap = require('./../simple-imap');
+
+var options = {
+	user: '<your username>',
+	password: '<password>',
+	host: '<imap host>',
+	port: 993,
+	tls: true,
+	mailbox: 'INBOX'
+};
+
+var simpleImap = new SimpleImap(options);
+
+simpleImap.on('error', function(err) {
+	console.log(err);
+});
+
+simpleImap.on('ready', function() {
+	console.log('ready');
+});
+
+simpleImap.on('open', function() {
+	console.log('mailbox opened');
+});
+
+simpleImap.on('mail', function(mail) {
+	console.log(mail);
+});
+
+simpleImap.on('close', function() {
+	console.log('closed');
+});
+
+simpleImap.on('end', function() {
+	console.log('end');
+});
+
+simpleImap.start();
---
+++ simple-imap.js
@@ -0,0 +1,92 @@
+var Imap = require('imap'),
+	MailParser = require('mailparser').MailParser,
+	moment = require('moment')
+	util = require('util'),
+	events = require('events');
+
+var SimpleImap = function(options) {
+	this.options = options;
+	this.imap = null;
+	
+	this.start = function() {
+		if (this.imap === null) {
+			this.imap = new Imap(this.options);
+
+			var selfImap = this.imap,
+				self = this;
+
+			selfImap.on('ready', function() {
+				self.emit('ready');
+
+				selfImap.openBox(self.options.mailbox, false, function() {
+					self.emit('open');
+				});
+			});
+
+			selfImap.on('mail', function(num) {
+				selfImap.search(['UNSEEN'], function(err, result) {
+					if (result.length) {
+						var f = selfImap.fetch(result, {
+							markSeen: true,
+							struct: true,
+							bodies: ''
+						});
+
+						f.on('message', function(msg, seqNo) {
+							msg.on('body', function(stream, info) {
+								var buffer = '';
+
+								stream.on('data', function(chunk) {
+									buffer += chunk.toString('utf8');
+								});
+
+								stream.on('end', function() {
+									var mailParser = new MailParser();
+									
+									mailParser.on('end', function(mailObject) {
+										self.emit('mail', {
+											from: mailObject.from,
+											subject: mailObject.subject,
+											text: mailObject.text,
+											html: mailObject.html,
+											date: moment(mailObject.date).format('YYYY-MM-DD HH:mm:ss')
+										});
+									});
+
+									mailParser.write(buffer);
+									mailParser.end();
+								});
+							});
+						});
+					}
+				});
+			});
+
+			selfImap.on('end', function() {
+				self.emit('end');
+			});
+
+			selfImap.on('error', function(err) {
+				self.emit('error', err);
+			});
+			
+			selfImap.on('close', function(hadError) {
+				self.emit('close', hadError);
+			});
+		}
+		
+		this.imap.connect();
+	}
+	
+	this.stop = function() {
+		this.imap.destroy();
+	}
+	
+	this.getImap = function() {
+		return this.imap;
+	}
+};
+
+util.inherits(SimpleImap, events.EventEmitter);
+
+module.exports = SimpleImap
